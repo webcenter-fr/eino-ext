2301f041b3b14efc8e1daa2868c3f6e04ca5e3ce
disaster37 <linuxworkgroup@hotmail.com>
2026-04-02 09:40:04 +0000

    fic: fix crd tools

--- components/tool/kubernetes/crd_describe.go
+++ components/tool/kubernetes/crd_describe.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/cloudwego/eino/components/tool"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
 )
 
 const customResourceDefinitionDescribeDescription = `
@@ -17,5 +19,8 @@ It return a JSON object representing the kubernetes CustomResourceDefinition
 
 // NewCustomResourceDefinitionDescribeTool creates a new instance of the CustomResourceDefinitionDescribeTool. It takes a context and a Configs object as parameters, builds Kubernetes clients for the provided configurations, and infers the tool using the description and invoke function. It returns the invokable tool or an error if any step fails.
 func NewCustomResourceDefinitionDescribeTool(ctx context.Context, configs Configs) (tool.InvokableTool, error) {
-	return NewDescribeTool(ctx, configs, "kubernetes_describe_customresourcedefinition", customResourceDefinitionDescribeDescription, &apiextensionsv1.CustomResourceDefinition{}, nil)
+	s := scheme.Scheme
+	utilruntime.Must(apiextensionsv1.AddToScheme(s))
+
+	return NewDescribeTool(ctx, configs, "kubernetes_describe_customresourcedefinition", customResourceDefinitionDescribeDescription, &apiextensionsv1.CustomResourceDefinition{}, s)
 }
--- components/tool/kubernetes/crd_list.go
+++ components/tool/kubernetes/crd_list.go
@@ -6,6 +6,8 @@ import (
 	"github.com/cloudwego/eino/components/tool"
 	"github.com/goccy/go-json"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
 )
 
 const customResourceDefinitionListDescription = `
@@ -49,5 +51,8 @@ func (h *CustomResourceDefinitionListOutput) ToJson(o *apiextensionsv1.CustomRes
 
 // NewCustomResourceDefinitionListTool creates a new instance of the CustomResourceDefinitionListTool. It takes a context and a Configs object as parameters, builds Kubernetes clients for the provided configurations, and infers the tool using the description and invoke function. It returns the invokable tool or an error if any step fails.
 func NewCustomResourceDefinitionListTool(ctx context.Context, configs Configs) (tool.InvokableTool, error) {
-	return NewListTool(ctx, configs, "kubernetes_list_customresourcedefinitions", customResourceDefinitionListDescription, &apiextensionsv1.CustomResourceDefinitionList{}, &CustomResourceDefinitionListOutput{}, nil)
+	s := scheme.Scheme
+	utilruntime.Must(apiextensionsv1.AddToScheme(s))
+
+	return NewListTool(ctx, configs, "kubernetes_list_customresourcedefinitions", customResourceDefinitionListDescription, &apiextensionsv1.CustomResourceDefinitionList{}, &CustomResourceDefinitionListOutput{}, s)
 }
