7054616f2540f98fa69ac84bfd71fd4faf7b0b33
disaster37 <linuxworkgroup@hotmail.com>
2026-04-02 10:09:53 +0000

    fix: fix resource list

--- components/tool/kubernetes/resource_list.go
+++ components/tool/kubernetes/resource_list.go
@@ -146,7 +146,7 @@ func (t *ResourceListTool) Invoke(ctx context.Context, params *ResourceListParam
 	}
 	o, err := c.Resource(namespaceResource).Namespace(params.Namespace).List(ctx, listOpts)
 	if err != nil {
-		return "", errors.Wrapf(err, "failed to list resources on namespace %s of type %s.%s", params.Namespace, params.ResourceKind, params.ResourceGroupVersion)
+		return "", errors.Wrapf(err, "failed to list resources on namespace %s of type %s.%s/%s", params.Namespace, params.ResourceKind, params.ResourceGroup, params.ResourceVersion)
 	}
 
 	outputs := make([]json.RawMessage, 0, len(o.Items))
